91cde406aa6fe7555b44f78dbcc0de625c18a348
YM <40844022+YM1998@users.noreply.github.com>
2020-11-03 23:44:41 -0500

    Add files via upload
    inicio proyecto

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+
+  {path:'',redirectTo:'/category', pathMatch:'full'},
+
+  { 
+      path: 'category', 
+      loadChildren: () => import('./views/category/category.module').then( m => m.CategoryModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
